8830ec971a9823cf576cf6cb0b96ca30ee472774
anandkulkarnisg <anand.kulkarni.sg@gmail.com>
2018-10-03 08:41:17 +0800

    Anand Kulkarni - 03/10/2018 - Added deleted constructors and removed reference to the boost noncopyable.

--- MessageDistributor.h
+++ MessageDistributor.h
@@ -4,14 +4,13 @@
 #include<mutex>
 #include<thread>
 #include<CyclicBarrier.h>
-#include<boost/core/noncopyable.hpp>
 
 #ifndef MessageDistributor_H
 #define MessageDistributor_H
 
 enum class MessageDistributorType { producer , consumer };
 
-template<typename T> class MessageDistributor : private boost::noncopyable
+template<typename T> class MessageDistributor
 {
 	private:
 		std::unique_ptr<CyclicBarrier> m_preBarrierPtr;						// We attempt to use a Flex Barrier to implement an MessageDistributor item.
@@ -19,6 +18,8 @@ template<typename T> class MessageDistributor : private boost::noncopyable
 		T m_MessageItem;													// Underlying data of the MessageDistributor.
 
 	public:
+		MessageDistributor(const MessageDistributor&) = delete;				// Copy deleted.
+		MessageDistributor& operator=(const MessageDistributor&) = delete;  // Assignment operator deleted.
 		MessageDistributor(const int&);										// Default Constructor. Takes number of parties.
 		void publish(const T&);												// Publishes a given item to the waiting consumer threads over the barrier sync.
 		T recieve();														// Recieves the published item in all the reciever threads over the barrier sync.
